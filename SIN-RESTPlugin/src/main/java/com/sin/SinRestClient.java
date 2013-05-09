fec9136183bceb7649e5168f08115bd22c0dd3b1
yogeshd <yogeshd@yogeshd-laptop.cybage.com>
2013-05-09 16:48:10 +0530

    trying this

--- SIN-RESTPlugin/src/main/java/com/sin/SinRestClient.java
+++ SIN-RESTPlugin/src/main/java/com/sin/SinRestClient.java
@@ -10,7 +10,7 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
-
+//test this
 public class SinRestClient {
 	
 	HttpClient httpClient;
